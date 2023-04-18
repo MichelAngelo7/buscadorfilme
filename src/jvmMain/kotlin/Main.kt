96c7c6e2a3a862b91ea0348dd48da0537418b0e4
michel.guimaraes <michel.guimaraes@sankhya.com.br>
2023-04-18 05:57:08 -0300

    Segundo dia do desafio #7DaysOfCode

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,25 +1,39 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.skia.Image
+import java.net.URL
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    val titulo = "Super Mario Bros. - O Filme"
+    val nota = 7.4
+    val ano = 2023
+    val url =
+        "https://m.media-amazon.com/images/M/MV5BZjA3MWM3MjMtYjk5Yy00NWRhLWIzM2ItYWY1MjcyNGIyYWU2XkEyXkFqcGdeQXVyMTU2MjY2Mjk3._V1_FMjpg_UX240_.jpg%20240w,%20https://m.media-amazon.com/images/M/MV5BZjA3MWM3MjMtYjk5Yy00NWRhLWIzM2ItYWY1MjcyNGIyYWU2XkEyXkFqcGdeQXVyMTU2MjY2Mjk3._V1_FMjpg_UY339_.jpg%20240w"
+
+
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Text(titulo)
+            Image(
+                contentDescription = titulo,
+                bitmap = url.loadImageBitmapByURL(),
+                modifier = Modifier.width(200.dp)
+            )
+            Text("Nota: $nota - Ano: $ano")
         }
     }
 }
@@ -28,4 +42,9 @@ fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
+}
+
+fun String.loadImageBitmapByURL(): ImageBitmap{
+    val inputStream = URL(this).readBytes()
+    return Image.makeFromEncoded(inputStream).toComposeImageBitmap()
 }
